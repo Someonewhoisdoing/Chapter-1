881789beebc932205f6cef275abeb337cd80d88b
Olga Ivanova <44057771+Someonewhoisdoing@users.noreply.github.com>
2019-12-15 23:53:57 +0300

    create HelloNameRunner.java

---
+++ projects/first/HelloNameRunner.java
@@ -0,0 +1,11 @@
+package first;
+
+/**
+ * Приветствовать любого пользователя при вводе его имени через командную строку
+ */
+
+public class HelloNameRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello, " + args[0]);
+    }
+}
